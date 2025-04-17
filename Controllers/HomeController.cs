b5f3045e45a5a09b384854aebab59626a9a6f848
vasilis <billaras345@gmail.com>
2025-04-17 12:08:34 +0300

    Home Page Created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 using System.Diagnostics;
+using Hotel.Application.Common.Interfaces;
 using Hotel_Management_System.Models;
+using Hotel_Management_System.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel_Management_System.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM home = new()
+            {
+                SuiteList = _unitOfWork.Suite.GetAll(includeProperties:"Suite_Amenity"),
+            };
+            return View(home);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using Hotel.Domain.Entities;
+
+namespace Hotel_Management_System.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Suite>? SuiteList { get; set; }
+
+    }
+}
